d1954bc4b4bf34f239d5bb8156a1af96467c07bc
Daeik Kim <daeik1222@gmail.com>
2018-11-07 18:14:28 +0900

    [Fix] Improper URL query parsing
    - Changed to remove URL parts after hash(#)

--- app/util/urlQuery.js
+++ app/util/urlQuery.js
@@ -2,7 +2,10 @@ module.exports = function() {
 
 	// Since SPA(Single Page App) might use hashbang(/#/?hello=world) in its url,
 	// `location.href` is being used to find query string.
-	const [, search] = location.href.split('?');
+	const [, queryAfter = ''] = location.href.split('?');
+
+	// Remove URL hash
+	const [search] = queryAfter.split('#');
 
 	if (!search) return {};
 
