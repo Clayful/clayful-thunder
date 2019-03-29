5f1fba5d9229acfc95b538129f33355fd0a133d7
Daeik Kim <daeik1222@gmail.com>
2019-03-29 10:52:20 +0900

    Bug Fixes and Improvements
    - [Fix] `components/checkout`: Fixed a bug where bundle items are not displayed in item listing
    - [Fix] `plugins/searchAddress/daum`: When an address doesn't include `city` value, default value will be used from `state` value
    - [Feature] `plugins/searchAddress/daum`: `iframe` style widget supports (`type` option)
    - [Feature] `plugins/searchAddress/daum`: Options for `daum.Postcode` constructor can be passed with plugin configurations

--- plugins/searchAddress/daum.js
+++ plugins/searchAddress/daum.js
@@ -4,18 +4,115 @@
  * - Guide: http://postcode.map.daum.net/guide
  */
 
-const implementation = () => {
+const implementation = (options = {}) => {
 
-	return callback => new daum.Postcode({
-		oncomplete: data => callback(null, {
-			postcode: data.zonecode || data.postcode,
-			country:  'KR', // Since Daum API only supports for Korean addresses, default value is always 'KR'
-			state:    data.sido,
-			city:     data.sigungu.split(' ')[0],
-			address1: data.address,
-			address2: '',
-		})
-	}).open();
+	options = $.extend({
+		type:    'popup',
+		options: {}
+	}, options);
+
+	return callback => {
+
+		const onFinish = [];
+
+		const windowWidth = $(window).width();
+		const windowHeight = $(window).height();
+
+		let width = 500;
+		let height = 500;
+
+		if (windowWidth > 500) {
+			width = 500;
+		} else if (windowWidth * 0.9 < 300) {
+			width = 300;
+		} else {
+			width = parseInt(windowWidth * 0.9);
+		}
+
+		if (windowHeight > 500) {
+			height = 500;
+		} else if (windowHeight * 0.9 < 400) {
+			height = 400;
+		} else {
+			height = parseInt(windowHeight * 0.9);
+		}
+
+		const widget = new daum.Postcode($.extend(
+			options.options,
+			{
+				oncomplete: data => {
+
+					onFinish.forEach(fn => fn());
+
+					return callback(null, {
+						postcode: data.zonecode || data.postcode,
+						country:  'KR', // Since Daum API only supports for Korean addresses, default value is always 'KR'
+						state:    data.sido,
+						city:     data.sigungu.split(' ')[0] || data.sido,
+						address1: data.address,
+						address2: '',
+					});
+				}
+			},
+			options.type === 'overlay' ? {
+				width:  width,
+				height: height
+			} : {}
+		));
+
+		if (options.type === 'popup') {
+			widget.open();
+		}
+		if (options.type === 'overlay') {
+
+			const container = $(`<div class="thunder--daum-search-address">
+				<span class="thunder--daum-search-address-close">×</span>
+				<div class="thunder--daum-search-address-wrap"></div>
+			</div>`);
+			const close = container.find('.thunder--daum-search-address-close');
+			const wrap = container.find('.thunder--daum-search-address-wrap');
+			const posTop = (windowHeight - (height + 36)) / 2;
+			const posLeft = (windowWidth - width) / 2;
+			const shadow = '#000 0 10px 26px -28px';
+
+			container.css({
+				position:             'fixed',
+				'z-index':            99999,
+				top:                  posTop,
+				left:                 posLeft,
+				'text-align':         'right',
+				'background-color':   '#333',
+				border:               '#dadada 1px solid',
+				'border-radius':      '4px',
+				color:                '#FFF',
+				'-webkit-box-shadow': shadow,
+				'-moz-box-shadow':    shadow,
+				'box-shadow':         shadow,
+			});
+			close.css({
+				display:          'inline-block',
+				'font-size':      '22px',
+				'padding-bottom': '7px',
+				'margin-right':   '8px',
+				cursor:           'pointer',
+			});
+
+			container.on('click', '.thunder--daum-search-address-close', () => {
+				return container.remove();
+			});
+
+			container.hide();
+
+			$('body').append(container);
+
+			widget.embed(wrap[0]);
+
+			container.show();
+
+			onFinish.push(() => container.remove());
+		}
+
+	};
 
 };
 
