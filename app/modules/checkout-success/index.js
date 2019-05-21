5931e79a3f326554eb341b9309b5bd34d96f6da3
Kim Dongjun <dongjun.kim@clayful.io>
2019-05-21 12:54:13 +0900

    [업데이트] 가상계좌 디스플레이, 주문번호 복사 버튼

--- app/modules/checkout-success/index.js
+++ app/modules/checkout-success/index.js
@@ -31,6 +31,20 @@ module.exports = Thunder => {
 		};
 
 		// Get an order or a subscription
+		if (type === 'order') {
+			return Thunder.request({
+				method: 'PUT',
+				url:    `/v1/me/${type}s/${subject}/transactions`
+			}).then(() => {
+				Thunder.request({
+					method: 'GET',
+					url:    `/v1/me/${type}s/${subject}`
+				}).then(subject => {
+					return callback(null, set(context, 'subject', subject));
+				});
+			});
+		}
+
 		return Thunder.request({
 			method: 'GET',
 			url:    `/v1/me/${type}s/${subject}`
@@ -47,14 +61,16 @@ module.exports = Thunder => {
 
 		const $container = $(this);
 		const $viewMoreDetails = $(this).find('.thunder--view-more-details');
-		const $copyToId = $(this).find('.copy-to-id');
-		const $copyToIdButton = $(this).find('.copy-to-id-button');
+		const $copyToId = $(this).find('.thunder--copy-to-id');
+		const $copyToIdButton = $(this).find('.thunder--copy-to-id-button');
 
 		$viewMoreDetails.on('click', () => Thunder.execute(
 			context.options.onViewDetails,
 			$container,
 			context
 		));
+
+		console.log('context', context);
 
 		$copyToIdButton.on('click', () => {
 			Thunder.util.copyToClipboard($copyToId);
